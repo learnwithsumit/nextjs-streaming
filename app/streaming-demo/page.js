c1c4a0e1d7e1b501ae4e63a731f2058e4b3998ce
Sumit Saha <sumit@learnwithsumit.com>
2025-07-27 19:56:48 +0600

    Custom Streaming Final Code

--- app/streaming-demo/page.js
+++ app/streaming-demo/page.js
@@ -1,6 +1,8 @@
 import ToolsCards from "@/components/tools-cards";
 
-export default function Home() {
+export const dynamic = "force-dynamic";
+
+export default function StreamingDemo() {
     return (
         <div className="w-full min-h-screen flex justify-center items-center">
             <ToolsCards />
